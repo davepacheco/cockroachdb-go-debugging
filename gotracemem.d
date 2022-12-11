a90bfa33a7d7ef4eb6822ea4ca2238b6b9276fc8
David Pacheco <dap@oxidecomputer.com>
2022-12-11 14:07:34 -0800

    tracing script used the last few days; includes tids, signal info

--- gotracemem.d
+++ gotracemem.d
@@ -39,12 +39,13 @@ BEGIN
 pid$target::runtime*mallocgc:entry
 {
 	self->size = *(uint64_t*)copyin(uregs[R_RSP]+sizeof (uint64_t), sizeof (uint64_t));
+	printf("dap: tid %d mallocgc entry (size 0x%x)\n", tid, self->size);
 }
 
 pid$target::runtime*mallocgc:return
 /self->size/
 {
-	printf("dap: alloc size 0x%x = 0x%x\n", self->size, uregs[R_RAX]);
+	printf("dap: tid %d mallocgc return (size 0x%x) = 0x%x\n", tid, self->size, uregs[R_RAX]);
 	self->size = 0;
 }
 
@@ -56,21 +57,21 @@ pid$target::runtime*sweepLocked*sweep:entry
 	this->span_locked = 0;
 	this->span = (mspan_t *)copyin(self->spanptr, sizeof (mspan_t));
 
-	printf("dap: span %p: %s\n", self->spanptr, "begin sweep");
-	printf("dap: span %p: %s: allocCount = %d (0x%x)\n", self->spanptr, "begin sweep", this->span->allocCount, this->span->allocCount);
-	printf("dap: span %p: %s: freeindex = %d (0x%x)\n", self->spanptr, "begin sweep", this->span->freeindex, this->span->freeindex);
-	printf("dap: span %p: %s: sweepgen = %d (0x%x)\n", self->spanptr, "begin sweep", this->span->sweepgen, this->span->sweepgen);
-	printf("dap: span %p: %s: state = %d (0x%x)\n", self->spanptr, "begin sweep", this->span->state, this->span->state);
-	printf("dap: span %p: %s: allocCache = 0x%x\n", self->spanptr, "begin sweep", this->span->allocCache);
-	printf("dap: span %p: %s: range [ %p, %p )\n", self->spanptr, "begin sweep", this->span->startAddr, this->span->limit);
-	printf("dap: span %p: %s: nelems = %d (0x%x)\n", self->spanptr, "begin sweep", this->span->nelems, this->span->nelems);
-	printf("dap: span %p: %s: elemsize = %d (0x%x)\n", self->spanptr, "begin sweep", this->span->elemsize, this->span->elemsize);
-	printf("dap: span %p: %s: npages = %d\n", self->spanptr, "begin sweep", this->span->npages);
-	printf("dap: span %p: allocBits:\n", self->spanptr);
+	printf("dap: tid %d span %p: %s\n", tid, self->spanptr, "begin sweep");
+	printf("dap: tid %d span %p: %s: allocCount = %d (0x%x)\n", tid, self->spanptr, "begin sweep", this->span->allocCount, this->span->allocCount);
+	printf("dap: tid %d span %p: %s: freeindex = %d (0x%x)\n", tid, self->spanptr, "begin sweep", this->span->freeindex, this->span->freeindex);
+	printf("dap: tid %d span %p: %s: sweepgen = %d (0x%x)\n", tid, self->spanptr, "begin sweep", this->span->sweepgen, this->span->sweepgen);
+	printf("dap: tid %d span %p: %s: state = %d (0x%x)\n", tid, self->spanptr, "begin sweep", this->span->state, this->span->state);
+	printf("dap: tid %d span %p: %s: allocCache = 0x%x\n", tid, self->spanptr, "begin sweep", this->span->allocCache);
+	printf("dap: tid %d span %p: %s: range [ %p, tid, %p )\n", tid, self->spanptr, "begin sweep", this->span->startAddr, this->span->limit);
+	printf("dap: tid %d span %p: %s: nelems = %d (0x%x)\n", tid, self->spanptr, "begin sweep", this->span->nelems, this->span->nelems);
+	printf("dap: tid %d span %p: %s: elemsize = %d (0x%x)\n", tid, self->spanptr, "begin sweep", this->span->elemsize, this->span->elemsize);
+	printf("dap: tid %d span %p: %s: npages = %d\n", tid, self->spanptr, "begin sweep", this->span->npages);
+	printf("dap: tid %d span %p: allocBits:\n", tid, self->spanptr);
 	this->nbytes_of_bits = (this->span->nelems + 7) / 8;
 	this->bits = copyin(this->span->allocBits, this->nbytes_of_bits);
 	tracemem(this->bits, 128, this->nbytes_of_bits);
-	printf("dap: span %p: gcmarkBits:\n", self->spanptr);
+	printf("dap: tid %d span %p: gcmarkBits:\n", tid, self->spanptr);
 	this->bits = copyin(this->span->gcmarkBits, this->nbytes_of_bits);
 	tracemem(this->bits, 128, this->nbytes_of_bits);
 	this->bits = 0;
@@ -81,7 +82,7 @@ pid$target::runtime*sweepLocked*sweep:entry
 pid$target::runtime*sweepLocked*sweep:b46
 /self->spanptr/
 {
-	printf("dap: span %p: clobbering 0x%x\n", self->spanptr, uregs[R_R10]);
+	printf("dap: tid %d span %p: clobbering 0x%x\n", tid, self->spanptr, uregs[R_R10]);
 }
 
 /* function: runtime.(*sweepLocked).sweep */
@@ -90,17 +91,17 @@ pid$target::runtime*sweepLocked*sweep:return
 {
 	this->span = (mspan_t *)copyin(self->spanptr, sizeof (mspan_t));
 
-	printf("dap: span %p: %s\n", self->spanptr, "end sweep");
-	printf("dap: span %p: %s: allocCount = %d (0x%x)\n", self->spanptr, "end sweep", this->span->allocCount, this->span->allocCount);
-	printf("dap: span %p: %s: freeindex = %d (0x%x)\n", self->spanptr, "end sweep", this->span->freeindex, this->span->freeindex);
-	printf("dap: span %p: %s: sweepgen = %d (0x%x)\n", self->spanptr, "end sweep", this->span->sweepgen, this->span->sweepgen);
-	printf("dap: span %p: %s: state = %d (0x%x)\n", self->spanptr, "end sweep", this->span->state, this->span->state);
-	printf("dap: span %p: %s: allocCache = 0x%x\n", self->spanptr, "end sweep", this->span->allocCache);
-	printf("dap: span %p: %s: range [ %p, %p )\n", self->spanptr, "end sweep", this->span->startAddr, this->span->limit);
-	printf("dap: span %p: %s: nelems = %d (0x%x)\n", self->spanptr, "end sweep", this->span->nelems, this->span->nelems);
-	printf("dap: span %p: %s: elemsize = %d (0x%x)\n", self->spanptr, "end sweep", this->span->elemsize, this->span->elemsize);
-	printf("dap: span %p: %s: npages = %d\n", self->spanptr, "end sweep", this->span->npages);
-	printf("dap: span %p: allocBits:\n", self->spanptr);
+	printf("dap: tid %d span %p: %s\n", tid, self->spanptr, "end sweep");
+	printf("dap: tid %d span %p: %s: allocCount = %d (0x%x)\n", tid, self->spanptr, "end sweep", this->span->allocCount, this->span->allocCount);
+	printf("dap: tid %d span %p: %s: freeindex = %d (0x%x)\n", tid, self->spanptr, "end sweep", this->span->freeindex, this->span->freeindex);
+	printf("dap: tid %d span %p: %s: sweepgen = %d (0x%x)\n", tid, self->spanptr, "end sweep", this->span->sweepgen, this->span->sweepgen);
+	printf("dap: tid %d span %p: %s: state = %d (0x%x)\n", tid, self->spanptr, "end sweep", this->span->state, this->span->state);
+	printf("dap: tid %d span %p: %s: allocCache = 0x%x\n", tid, self->spanptr, "end sweep", this->span->allocCache);
+	printf("dap: tid %d span %p: %s: range [ %p, tid, %p )\n", tid, self->spanptr, "end sweep", this->span->startAddr, this->span->limit);
+	printf("dap: tid %d span %p: %s: nelems = %d (0x%x)\n", tid, self->spanptr, "end sweep", this->span->nelems, this->span->nelems);
+	printf("dap: tid %d span %p: %s: elemsize = %d (0x%x)\n", tid, self->spanptr, "end sweep", this->span->elemsize, this->span->elemsize);
+	printf("dap: tid %d span %p: %s: npages = %d\n", tid, self->spanptr, "end sweep", this->span->npages);
+	printf("dap: tid %d span %p: allocBits:\n", tid, self->spanptr);
 	this->nbytes_of_bits = (this->span->nelems + 7) / 8;
 	this->bits = copyin(this->span->allocBits, this->nbytes_of_bits);
 	tracemem(this->bits, 128, this->nbytes_of_bits);
@@ -109,14 +110,57 @@ pid$target::runtime*sweepLocked*sweep:return
 	self->spanptr = 0;
 }
 
+/* see uts/intel/sys/regset.h */
+#define REG_FSBASE 26
+
+/* trace signal handling */
+
+/* signal handling begins on entry to sigacthandler */
+pid$target::sigacthandler:entry
+{
+	self->handling_signal++;
+	this->ucontext = (ucontext_t *)copyin(arg2, sizeof (ucontext_t));
+	printf("dap: tid %d sigacthandler entry depth %d sig %d ucontext fsbase = %p kernel view of fsbase = %p\n",
+	    tid, self->handling_signal, arg0, this->ucontext->uc_mcontext.gregs[REG_FSBASE],
+	    curthread->t_lwp->lwp_pcb.pcb_fsbase);
+}
+
+pid$target::runtime.*sigtrampgo:entry,
+pid$target::runtime.*sigtrampgo:return
+{
+	printf("dap: tid %d runtime.sigtrampgo.%s (depth = %d)\n",
+	    tid, probename, self->handling_signal);
+}
+
+/*
+ * signal handling ends with setcontext() syscall, which seems only to be used
+ * during signal handling in our case
+ */
+syscall::setcontext:entry
+/pid == $target && arg0 == 1/
+{
+	this->ucontext = (ucontext_t *)copyin(arg1, sizeof (ucontext_t));
+	printf("dap: tid %d setcontext entry depth %d ucontext fsbase = %p kernel view of fsbase = %p\n",
+	    tid, self->handling_signal, this->ucontext->uc_mcontext.gregs[REG_FSBASE],
+	    curthread->t_lwp->lwp_pcb.pcb_fsbase);
+}
+
+syscall::setcontext:entry
+/pid == $target && arg0 == 1 && self->handling_signal > 0/
+{
+	self->handling_signal--;
+}
+
+/* actions at process exit */
+
 syscall::rexit:entry
 /pid == $target/
 {
-	printf("dap: exit: %d\n", arg0);
+	printf("dap: tid %d exit: %d\n", tid, arg0);
 }
 
 syscall::write:entry
 /pid == $target && arg0 <= 2/
 {
-	printf("dap: write fd %d %d bytes\n", arg0, arg2);
+	printf("dap: tid %d write fd %d %d bytes\n", tid, arg0, arg2);
 }
